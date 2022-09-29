25be5233da4d05be625239ceeab64a8f1f4d35c6
gasawase <gasawase@mail.uc.edu>
2022-09-29 16:34:39 -0400

    Initial Push

---
+++ VRComputerRepair/Assets/Scripts/ProgressTracker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressTracker : MonoBehaviour
+{
+    //get list of steps
+    //go through list of steps to see if they're done
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
